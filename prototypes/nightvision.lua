5f25b55022cbfb312ccb1b059ceefa856be04f7c
Lebothegizebo <daltonliam321@gmail.com>
2024-10-22 21:36:49 +1100

    Bug Fixes

--- prototypes/nightvision.lua
+++ prototypes/nightvision.lua
@@ -1,3 +1,3 @@
 if settings.startup["enable-night-vison-restore"].value then
-    data.raw["night-vision-equipment"]["night-vision-equipment"].color_lookup = {{0.5, "__restore-night-vison__/graphics/color_luts/nightvision.png"}}
+    data.raw["night-vision-equipment"]["night-vision-equipment"].color_lookup = {{0.5, "__restore-night-vision__/graphics/color_luts/nightvision.png"}}
 end
